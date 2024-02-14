7e04addcb2703551225139021209893e474ea5e7
loghder <loghder@gmail.com>
2024-02-14 15:42:27 +0800

    增加睡眠时切换壁纸的功能

--- Spoons/IdolDaily.spoon/init.lua
+++ Spoons/IdolDaily.spoon/init.lua
@@ -4,15 +4,15 @@ Date: 2024-02-13 00:35:48
 LastEditors: Loghder loghder@qq.com
 LastEditTime: 2024-02-13 06:53:11
 FilePath: /.hammerspoon/Spoons/BingDaily.spoon/init.lua
-Description: 
+Description:
 
-Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
+Copyright (c) 2024 by ${git_name_email}, All Rights Reserved.
 --]]
 --- === IdolDaily ===
 ---
 --- Use Idol daily picture as your wallpaper, automatically.
 
-local obj={}
+local obj = {}
 obj.__index = obj
 
 -- Metadata
@@ -26,13 +26,20 @@ local function bingRequest()
     hs.shortcuts.run("设置本地壁纸")
 end
 
+local function iDolDaily(eventType)
+    if (eventType == hs.caffeinate.watcher.screensDidSleep) then
+        hs.shortcuts.run("设置本地壁纸")
+    end
+end
 function obj:init()
     if obj.timer == nil then
-        obj.timer = hs.timer.doEvery(3*60*60, function() bingRequest() end)
+        obj.timer = hs.timer.doEvery(3 * 60 * 60, function() bingRequest() end)
         obj.timer:setNextTrigger(5)
     else
         obj.timer:start()
     end
+    local watcher = hs.caffeinate.watcher.new(iDolDaily)
+    watcher:start()
 end
 
 return obj
